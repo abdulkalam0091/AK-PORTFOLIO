2562254b041c89b83cf0ead72c1bc87d2784b92e
abdulkalam0091 <kalam889136@gmail.com>
2024-09-16 17:11:19 +0530

    style.css

--- index.js
+++ index.js
@@ -1,210 +1,567 @@
-let a=2;
-let b=5;
-if (a % 2 === 0) {
-    console.log("a is even.");
-} else {
-    console.log("a is odd.");
-}
+//let a=7;
+//if(a==7){
+    //console.log("dhoni");
+//}
 
-if (b % 2 === 0) {
-    console.log("b is even.");
-} else {
-    console.log("b is odd.");
-}
+    //else if{
+        //console.log(dhoni);
+    //}
 
+//let age = 18;
 
-
-
-
+//if (age >= 18) {
+    //console.log("You are an adult.");
+//} else {
+    //console.log("You are a minor.");
+//}
 
 
+//let p=10000;
+//let n=3;
+//let r=8;
+//let si;
+//si(p*n*r);
 
-
-
-
-
-
-
-
-
-
-
-
+//if(si<50){
+    //console.log("50 greaterthan");
+//}
+//else{
+//console.log("50 lessthan");
+//}
 
 
 
+//let count=1;
+//while(count<10){
+    //console.log("value of counters+count");
+    //count++;
+//}
 
 
-let method=10;
-let teamscore=150;
-let targetscore=200;
-{
-    if (teamscore>targetscore)
-}
-console.log(teamloss target score);
-{
-    else if(teamscore < targetscore && overleft >0)
-}
-else if(teamscore <targetscore && overleft===0){
-}
-
+//let rno=1001;
+//while (rno<1027){
+    //console.log("rno"+rno);
+   // rno++;
+//}
 
 
 
+//let numbur=50;
+//while(numbur<60){
+    //console.log("numbur"+numbur);
+    //numbur++;
+//}
 
 
 
+//  let table=2;
+// while(table<13){
+//      console.log("table"+table);
+//      table++;
+// }
+ 
 
+// 
 
+// let count=1;
+// while(count<6){
+//     console.log(2*count);
+//     count++;
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fucnction is(leapyear);{
-let y=4;
-let n=100;
-let y=400;
-if (a % 4 === 0) {
-if (a % 100 === 0)
-if (a % 400 ===0 ) 
-    return "Y";
-            } else {
-                return "N";
-            }
-        } else {
-            return "Y";
-        }
-    } else {
-        return "N";
-    }
-    console.log(leapyear(2000));
-    console.log(leapyear(1900));
-    console.log(leapyear (2020));  
+// let count=1;
+// while(count<=15){
+// console.log("value of count before increament count",count)
+// count++;
+// console.log("value of count increament count ",count)
+//     console.log("2*"+count,"=", (2*count))
     
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio Projects</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <section class="projects">
-        <div class="project" id="project3">
-            <h2>Project 3</h2>
-            <h3>Fitness Mobile App Design</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-            <a href="#">Read more</a>
-            <div class="project-image">
-                <img src="fitness-app.png" alt="Fitness Mobile App Design">
-            </div>
-        </div>
+// }
 
-        <div class="project" id="project4">
-            <h2>Project 4</h2>
-            <h3>Modelling Mobile App Design</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-            <a href="#">Read more</a>
-            <div class="project-image">
-                <img src="modeling-app.png" alt="Modelling Mobile App Design">
-            </div>
-        </div>
-    </section>
 
-    <div class="view-all">
-        <a href="#">View all</a>
-    </div>
-</body>
-</html>
+// function printable(tablevalue){
+//     let count=1;
+//      while(count<=15){
+//      console.log("value of count before increament count",count)
+//      count++;
+//     console.log("value of count increament count ",count)
+//          console.log("2*"+count,"=", (2*count))
+        
+// }
+
+
+// printable(3,100)
+// function printable(tablevalue,numbur){
+//     console.log("the value of table"+table value);
+//     console.log("table value of numbur"+numbur);
+//     let count=1;
+//     while (while<numbur){
+//         console.log(tablevalue+ "*" + count+ "="+ tablevalue*counter);
+//         count++;
+//     }
+
+// }
 
 
 
+// let no=2;
+// while(no<12){
+//     console.log("no"+no);
+//     no++;
+// }
 
-body {
-    font-family: Arial, sans-serif;
-    background-color: #0A0B23;
-    color: white;
-    margin: 0;
-    padding: 0;
+
+
+// 
+
+// let tbale=4;
+// while(table<=20){
+// console.log("value of table before increasement table" ,table);
+// count++;
+// console.log("value of table before incresement table",table);
+// console.log("4*" +table, "=", (4*table));
+// }
+
+
+// let value =2;
+// while(value<16){
+//     console.log("value+value");
+//     value++;
+// }
+
+
+// let table=2;
+// while(table<20){
+//     console.log("table"+table)
+//     table++;
+// }
+
+// value i = 10;
+// do {
+//     cout << i << "10 ";
+//     i--;
+// } while (i > 5);
+
+
+// let text = "2";
+// let i = 0;
+// do {
+//   text += i + "<br>";
+//   i++;
+// }
+// while (i < 5);
+
+
+// let value=1;
+// while(value<10){
+//     console.log("value"+value)
+//     value++;
+// }
+
+// let value=10;
+
+// while (value<= 1){
+//     print(value)
+//     value-= 1;
+// }
+
+//  let value = 10;
+
+// while (value<10){
+//     console.log("value"-1);
+//     value++;
+// }
+
+// n = 5  
+
+// for i in range(n, 0, -1):
+// print(i, end="1")
+
+// Given 2 numbers a and b (a < b). Print all the
+// numbers between a and b.
+// a = 5, b = 8
+// 5
+// 6
+// 7
+// 8
+
+// let a=5;
+// let b=10;
+// while(a<b){
+//     a--;
+//  console.log(a);
+
+// }
+
+
+// let a=6;
+// let b=20;
+// while(a<b){
+//     a++;
+//     console.log(a);
+// }
+
+
+// 
+
+
+// console.log(addtwonumbur(33,47));
+// function addtwonumbur (a,b){
+// return (a+b)
+// ;
+// }
+
+
+
+// console.log ( "myname"+printsomething("kalam"));
+// console.log("rollnumbur"+printsomething(12));
+// console.log ( "iam from"+printsomething("freshwork software academy"));
+
+// function printsomething(value){
+//     return value
+// }
+
+// let a=12;
+// while(a>24){
+//     console.log(a);
+//     a--;
+// }
+
+
+// let a=3000;b=1
+// function kalam(a,b){console.log("print")
+// }
+
+
+
+
+//     console.log(kalam(a,b))
+
+
+// let value=5;
+// while(value-15){
+//     console.log(value-value);
+//     value--;
+// }
+
+
+
+
+// total = 0;
+// i = 1;
+// while(total<1);
+// console.log(total+1);
+// total++;
+
+
+//    console.log("myname"+printsomething("abdul"));
+//    console.log("myage"+printsomething("21"));
+//    console.log("rollno"+printsomething("55"));
+
+//   function printsomething (value){
+//     return value
+//    }
+
+// function sumNumbers(1){
+//     let sum = 1;
+//     let i = 0;
+//     while (i < 1) {
+//       sum += i;
+//       i++;
+//     }
+//     return sum;
+// }
+
+// sumNumbur(n){
+//     let sum =1;
+//     let i=0;
+//     while(i<=1){
+//         sum+=0;
+//         console.log(sum)
+//         i++;
+//     }
+
+// }
+// console.log(sumNumbers(6));
+// function sumNumbers(n) {
+//     let sum = 0;
+//     let i = 0;
+//     while (i =<n) {
+//       sum += i;
+//       i++;
+//     }
+//     return sum;
+//   }
+
+
+// console.log(sumnumbur())
+// function sumnumbur(n){
+//     let sum= 0;
+//     let i = 0;
+//     while(i<n){
+//         sum +=i;
+
+//     }
+// }
+
+
+
+//  let value =10;
+// //  let value = 10;
+//  while(value>0){
+//  console.log(value);
+//  value--;
+//  }
+
+
+// let a=-1;
+// let b=10;
+//  while(b<a){
+//     console.log(b-10);
+//    a--;
+  
+//  }
+
+
+// function sumnumure(n){
+//     let sum =1;
+//     let i=0;
+//     while(i<n){
+//         console.log(sum);
+//         sum+=i;
+//         i++;
+//     }
+
+// }
+  
+
+// let value =-10;
+// while(value<-0){
+//     console.log(value)
+//     value--;
+// }
+
+// sumnumbur(5);
+// function sumnumbur(n){
+//     while(n>=0);
+//     console.log(n);
+//     n--;
+// }
+
+// factorial(6){
+//     function factorial(n){
+//         let sum=1;
+//         let i =0;
+//         while(factorial<=n);
+//         console.log(factorial)
+//         factorial*=n;
+        
+//     }
+// }
+// factorial(5)
+// function factorial(num) {
+//     let sum = 1;
+//     let i==0;
+//     for (let i = 2; i <= num; i++) {
+//       result *= i;
+//     }
+//     return result;
+//   }
+  
+//   console.log(factorial(5)); 
+
+// factoraial(5);
+// function factorial(num){
+//     let num =1;
+//     let i =0;
+//     while(i<=num);
+//     console.log(num)
+//     num*=i;
+//     num++;
+
+
+// }
+  
+// for(x=1;x<=5;x++){
+//     console.log(x);
+// }
+
+// for(x=5;x<=1;x--){
+//     console.log(x);
+// }
+
+// for(x=1;x<=5;x++){
+//     console.log(x);
+// }
+// sum=0
+// for(x=1;x<=5;x++){
+//     // sum=sum+1
+//     console.log(x);
+// }
+
+// add(6)
+// function add(x){
+//     x=x+2;
+//     console.log(x);
+//     return "hi"+x;
+// }
+
+// let value =1;
+// function value(x){
+//     while(value<101);
+//     console.log(value);
+//     value++;
+// }
+
+// let value=1;
+// function value(n){
+// while(value<=101){
+//     console.log(value);
+//     value++;
+// }if (value2%===0){
+// print('even numbur')
+// }else{
+//     print("odd numbur")
+// }
+
+// value(2);
+// function value(n){
+//     if(n2%===0){
+//         print("even numbur");
+//     }else{
+//         print("odd numbur");
+//     }
+// }
+// function print(n){
+//     console.logI(n);
+// }
+
+// let greeting = "Hello, World!";
+// let name = 'John Doe';
+// let message = `Welcome to the JavaScript world, ${name}!`;
+
+// console.log(greeting); 
+// console.log(name); 
+// console.log(message);  
+
+
+// let greeting= "hi,all,";
+// let name = 'abdul kalam';
+// let messege= "freshwork software academy",$(name)!';
+
+// console.log("greeting");
+// console.log("name");
+// console.log("messege");
+
+//  let value =1;
+//  while(value<10){
+//      console.log(value);
+//     value++;
+//  }
+
+// value (2);
+// function value(n){
+// let sum=0;
+// let i=1;
+// while(n<=1)
+//     console.log(n)
+// n++;
+// return result
+// }
+
+//  let value =1;
+//  while(value<10){
+//     console.log(value);
+//    value++;
+//  }
+
+
+
+
+// let a= 10;
+// let b= "8";
+
+// console.log(a+b);
+
+
+// let a= "kalam";
+// let b=10;
+// console.log(a++b);
+
+
+
+// let value=" ";
+// let i=1;
+// while(i<=5){
+//     value=value+i+" ";
+//     i++;
+    
+// }
+// console.log(value)
+
+
+// let value=" *";
+// let i=1;
+// while(i<=5){
+//     value=value+i+" ";
+//     console.log(value);
+//     i++;
+// }
+// console.log(value);
+
+
+value (1)
+functin value(n){
+console.log(n)
+if(n%2==0){
+    console.log("even numbur");
+}else{
+    console.log("odd numbur");
 }
 
-.projects {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-    padding: 50px 0;
 }
 
-.project {
-    background-color: #1C1D3F;
-    padding: 20px;
-    border-radius: 10px;
-    width: 80%;
-    max-width: 800px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-    text-align: center;
-    position: relative;
-}
 
-.project h2 {
-    color: #46B3F6;
-}
 
-.project h3 {
-    margin: 10px 0;
-    font-size: 1.5em;
-}
 
-.project p {
-    font-size: 1em;
-    line-height: 1.5em;
-    margin-bottom: 20px;
-}
 
-.project a {
-    color: #46B3F6;
-    text-decoration: none;
-    font-weight: bold;
-}
 
-.project-image {
-    margin-top: 20px;
-}
 
-.project-image img {
-    max-width: 100%;
-    border-radius: 10px;
-}
 
-.view-all {
-    text-align: center;
-    margin-top: 30px;
-}
 
-.view-all a {
-    background-color: #5A61F6;
-    color: white;
-    padding: 10px 20px;
-    border-radius: 5px;
-    text-decoration: none;
-    font-weight: bold;
-    transition: background-color 0.3s ease;
-}
 
-.view-all a:hover {
-    background-color: #46B3F6;
-}
+
+// let value="";
+// let i=5;
+// while(i<=30){
+//     value=value+i+"";
+//     i++;
+// }
+// console.log(value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // value (8)
+// function value(n) {
+//     let sum = 0;
+//     let i = 0;
+//     while (i < =n) {
+//         console.log(n);
+//       sum += i;
+//       i++;
+//     }
+//     return sum
+// }
